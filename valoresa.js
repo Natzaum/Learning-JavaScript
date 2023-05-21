6cb36da564456b5b65ffb4b359812413f3e37df3
Natzaum <natacezerbordignon@gmail.com>
2023-05-20 23:18:47 -0300

    valores a

---
+++ valoresa.js
@@ -0,0 +1,11 @@
+let A = 2
+let B = 3
+let C = 2
+let D = 6
+
+if(B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0){
+    console.log("valores aceitos")
+}
+else{
+    console.log("valores nao aceitos")
+}
